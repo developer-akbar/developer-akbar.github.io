cbbc7406c75001226c9523b350e3d15d9494153b
developer-akbar <akbar.ak219@gmail.com>
2024-02-10 13:12:52 +0530

    Removed

--- salah-timings/scripts/weather.js
+++ salah-timings/scripts/weather.js
@@ -19,7 +19,10 @@ async function getWeather() {
         const weatherData = await response.json();
 
         document.querySelector('.current-temp').textContent = Math.round(weatherData.current.temperature_2m);
-        document.querySelector('.weather-icon').src = getWeatherIconUrl(weatherData.current.weather_code);
+        document.querySelector('.current-weather-wrapper i').removeAttribute('class');
+        // adding weather icon dynamically based on Weather API response
+        document.querySelector('.current-weather-wrapper i').classList.add('fa-solid', 'weather-icon', getWeatherIconUrl(weatherData.current.weather_code));
+        
     } catch (err) {
         console.error('Unable to get weather information', err);
     } finally {
@@ -30,19 +33,19 @@ async function getWeather() {
 
 // utility function to get weather icon url based on weather code from icons folder
 function getWeatherIconUrl(weatherCode) {
-    return `icons/${WEATHER_ICON_MAP.get(weatherCode)}.svg`;
+    return `${WEATHER_ICON_MAP.get(weatherCode)}`;
 }
 
 const WEATHER_ICON_MAP = new Map();
 
 // mapping weather icon with weather codes
-addWeatherIconMapping([0, 1], "sun");
-addWeatherIconMapping([2], "cloud-sun");
-addWeatherIconMapping([3], "cloud");
-addWeatherIconMapping([45, 48], "smog");
-addWeatherIconMapping([51, 53, 56, 57, 61, 63, 65, 66, 67, 80, 81, 82], "cloud-showers-heavy");
-addWeatherIconMapping([71, 73, 75, 77, 85, 86], "snowflake");
-addWeatherIconMapping([95, 96, 99], "cloud-bolt");
+addWeatherIconMapping([0, 1], "fa-sun");
+addWeatherIconMapping([2], "fa-cloud-sun");
+addWeatherIconMapping([3], "fa-cloud");
+addWeatherIconMapping([45, 48], "fa-smog");
+addWeatherIconMapping([51, 53, 56, 57, 61, 63, 65, 66, 67, 80, 81, 82], "fa-cloud-showers-heavy");
+addWeatherIconMapping([71, 73, 75, 77, 85, 86], "fa-snowflake");
+addWeatherIconMapping([95, 96, 99], "fa-cloud-bolt");
 
 
 // utility function to map weather icon with specific weather code
